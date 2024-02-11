a8c6d017a38072d8f7dc064e89ee5238ad5686b3
Morris Brodersen <mb@morrisbrodersen.de>
2024-02-11 15:21:03 +0100

    test mongodb 5,6,7; simplify pipeline

--- scripts/maybe-publish.sh
+++
@@ -1,9 +0,0 @@
-NAME=$(node -p "require('./package.json').name")
-VERSION=$(node -p "require('./package.json').version")
-
-if [[ $(npm info $NAME@$VERSION) ]]; then
-  echo "$NAME@$VERSION already published, skipping"
-else
-  echo "Publishing $NAME@$VERSION..."
-  npm publish
-fi
